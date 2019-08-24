7edd8169132b29c50b0a79ca3f6463ba52029dfc
AlaaTaima <alaa.ktaima@gmail.com>
2019-08-25 01:34:07 +0300

    edit db-url and

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -3,11 +3,20 @@ const { Pool } = require('pg');
 
 require('env2')('./config.env');
 
-if (!process.env.DB_URL) throw new Error('Database URL Not Found!!');
+let DB_URL = '';
+
+if (process.env.NODE_ENV === 'development') {
+  DB_URL = process.env.DB_URL;
+} else {
+  DB_URL = process.env.DATABASE_URL;
+}
+
 
 const options = {
-  connectionString: process.env.DB_URL,
+  connectionString: DB_URL,
   ssl: true
 };
+
+if (!DB_URL) throw new Error('Database URL Not Found!!');
 
 module.exports = new Pool(options);
